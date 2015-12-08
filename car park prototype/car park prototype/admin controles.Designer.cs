898bf1950cf859b2bf319e71b3c8e07b48ae1529
liam <liam@king.com>
2015-12-08 12:58:41 +0000

    added emergency selector

--- car park prototype/car park prototype/admin controles.Designer.cs
+++ car park prototype/car park prototype/admin controles.Designer.cs
@@ -98,7 +98,7 @@
             this.listBox1.FormattingEnabled = true;
             this.listBox1.Location = new System.Drawing.Point(281, 119);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(163, 160);
+            this.listBox1.Size = new System.Drawing.Size(246, 160);
             this.listBox1.TabIndex = 6;
             // 
             // timer1
@@ -186,7 +186,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(456, 287);
+            this.ClientSize = new System.Drawing.Size(539, 287);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.textBox3);
--- car park prototype/car park prototype/customer controles.Designer.cs
+++ car park prototype/car park prototype/customer controles.Designer.cs
@@ -123,6 +123,7 @@
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 20);
             this.textBox1.TabIndex = 8;
+            this.textBox1.Text = "nv56 qyc";
             // 
             // label5
             // 
